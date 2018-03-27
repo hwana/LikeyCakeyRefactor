46d203ed8deb9e8b55b33c820563c26deff7421a
Young Kim <blue130830@gmail.com>
2018-03-27 12:09:09 +0900

    그냥 마스터거 다 넣어버림

--- src/main/webapp/resources/young/young.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $(window).scroll(function(){
-    var scrollTop = $(window).scrollTop();
-    if(scrollTop > 300 && scrollTop < 2000){
-    	$('.young-host-profile-box').css({transform: 'translateY(' + (scrollTop-300) +'px)'});
-    }
-  });
-});	
