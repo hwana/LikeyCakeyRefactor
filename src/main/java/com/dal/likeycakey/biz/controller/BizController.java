6e8f2f7cb3f6dcb184084047a729389d554c25ba
hwanhee <dsa1461@gmail.com>
2018-04-02 09:07:56 +0900

    biz join commit

--- src/main/java/com/dal/likeycakey/biz/controller/BizController.java
+++ src/main/java/com/dal/likeycakey/biz/controller/BizController.java
@@ -1,32 +1,27 @@
 package com.dal.likeycakey.biz.controller;
 
 
-import java.util.Locale;
+import java.sql.SQLException;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.dal.likeycakey.biz.model.service.BizService;
+import com.dal.likeycakey.biz.model.vo.BizMember;
+import com.dal.likeycakey.member.model.vo.Member;
 
-/**
- * 
- * Handles requests for the application home page.
- */
 @Controller
 public class BizController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(BizController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private BizService bizService;
 	
 	//사업자 로그인
 	@RequestMapping(value = "bizLogin.ca", method = RequestMethod.GET)
-	public String bizLogin(Locale locale, Model model) {
+	public String bizLogin() {
 		
 		
 		return "biz/bizLogin";
@@ -34,15 +29,33 @@ public class BizController {
 	
 	//사업자 회원가입
 	@RequestMapping(value = "bizJoin.ca", method = RequestMethod.GET)
-	public String bizJoin(Locale locale, Model model) {
+	public String bizJoin() {
 		
 		
 		return "biz/bizJoin";
 	}
 	
+	@RequestMapping(value = "bizInsert.ca", method = RequestMethod.GET)
+	public ModelAndView bizInsert(Member m, BizMember bm, ModelAndView mv) {
+		
+		try {
+			int result = bizService.insertBiz(m);
+			int result2 =bizService.insertBiz(bm);
+			System.out.println("비즈 등록 성공");
+			mv.setViewName("redirect:home.ca");
+		} catch (Exception e) {
+			System.out.println(e);
+			System.out.println("비즈 등록 실패");
+			mv.setViewName("redirect:home.ca");
+		}
+		return mv;
+	}
+	
+	
+	
 	//사업자 회원정보, 매장정보 수정
 	@RequestMapping(value = "bizMypageModify.ca", method = RequestMethod.GET)
-	public String bizMypageModify(Locale locale, Model model) {
+	public String bizMypageModify() {
 		
 		
 		return "biz/bizMypageModify";
@@ -50,7 +63,7 @@ public class BizController {
 	
 	//등록된 케이크가 하나도 없는 상태에서 케이크 등록하는 페이지
 	@RequestMapping(value = "nocakeUpload.ca", method = RequestMethod.GET)
-	public String nocakeUpload(Locale locale, Model model) {
+	public String nocakeUpload() {
 		
 		
 		return "biz/nocakeUpload";
@@ -59,7 +72,7 @@ public class BizController {
 
 	//등록한 케이크가 있는 상태에서 케이크 등록하는 페이지
 	@RequestMapping(value = "yescakeUpload.ca", method = RequestMethod.GET)
-	public String yescakeUpload(Locale locale, Model model) {
+	public String yescakeUpload() {
 		
 		
 		return "biz/yescakeUpload";
@@ -67,7 +80,7 @@ public class BizController {
 	
 	//완제품 케이크 등록 페이지
 	@RequestMapping(value = "productCakeUpload.ca", method = RequestMethod.GET)
-	public String productCakeUpload(Locale locale, Model model) {
+	public String productCakeUpload() {
 		
 		
 		return "biz/productCakeUpload";
@@ -75,7 +88,7 @@ public class BizController {
 	
 	//커스텀 케이크 등록 페이지
 	@RequestMapping(value = "customCakeUpload.ca", method = RequestMethod.GET)
-	public String customCakeUpload(Locale locale, Model model) {
+	public String customCakeUpload() {
 		
 		
 		return "biz/customCakeUpload";
@@ -83,7 +96,7 @@ public class BizController {
 	
 	//케이크 등록하기 버튼 클릭 후 완제품케이크를 등록할건지 커스텀 케이크를 등록할건지 선택하는 페이지
 	@RequestMapping(value = "pcSelect.ca", method = RequestMethod.GET)
-	public String pcSelect(Locale locale, Model model) {
+	public String pcSelect() {
 		
 		
 		return "biz/pcSelect";
@@ -92,7 +105,7 @@ public class BizController {
 	
 	//사업자에게 들어온 문의글 모아보는 페이지
 	@RequestMapping(value = "bizQNA.ca", method = RequestMethod.GET)
-	public String bizQNA(Locale locale, Model model) {
+	public String bizQNA() {
 		
 		
 		return "biz/bizQNA";
@@ -100,7 +113,7 @@ public class BizController {
 	
 	//"등록이 완료되었습니다"페이지
 	@RequestMapping(value = "uploadComplete.ca", method = RequestMethod.GET)
-	public String uploadOK(Locale locale, Model model) {
+	public String uploadOK() {
 		
 		
 		return "biz/uploadComplete";
@@ -108,7 +121,7 @@ public class BizController {
 	
 	//사업자 아이디 패스워드 찾는 페이지
 	@RequestMapping(value = "findIdPw.ca", method = RequestMethod.GET)
-	public String findIdPw(Locale locale, Model model) {
+	public String findIdPw() {
 		
 		
 		return "biz/findIdPw";
---
+++ src/main/java/com/dal/likeycakey/biz/model/vo/BizMember.java
@@ -0,0 +1,66 @@
+package com.dal.likeycakey.biz.model.vo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BizMember {
+	
+	private String id;
+	private String bizName; //매장이름
+	private String bizPn;
+	private String bizNum; //사업자 등록번호
+	private int bizDelivery; //배송비
+	private String masterName; //대표자 이름
+	
+	public BizMember() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public BizMember(String id, String bizName, String bizPn, String bizNum, int bizDelivery, String masterName) {
+		super();
+		this.id = id;
+		this.bizName = bizName;
+		this.bizPn = bizPn;
+		this.bizNum = bizNum;
+		this.bizDelivery = bizDelivery;
+		this.masterName = masterName;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getBizName() {
+		return bizName;
+	}
+	public void setBizName(String bizName) {
+		this.bizName = bizName;
+	}
+	public String getBizPn() {
+		return bizPn;
+	}
+	public void setBizPn(String bizPn) {
+		this.bizPn = bizPn;
+	}
+	public String getBizNum() {
+		return bizNum;
+	}
+	public void setBizNum(String bizNum) {
+		this.bizNum = bizNum;
+	}
+	public int getBizDelivery() {
+		return bizDelivery;
+	}
+	public void setBizDelivery(int bizDelivery) {
+		this.bizDelivery = bizDelivery;
+	}
+	public String getMasterName() {
+		return masterName;
+	}
+	public void setMasterName(String masterName) {
+		this.masterName = masterName;
+	}
+
+	
+}
