02f218e99d5b7106e4bb4c13aac7a6d0c08828a0
Young Kim <blue130830@gmail.com>
2018-04-04 10:03:38 +0900

    MemberService fix

--- src/main/java/com/dal/likeycakey/member/model/service/MemberService.java
+++ src/main/java/com/dal/likeycakey/member/model/service/MemberService.java
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-package com.dal.likeycakey.member.model.service;
-
-import com.dal.likeycakey.member.model.vo.Member;
-
-public interface MemberService {
-	// Controller에서 서비스를 수행하기 위한 메소드의 이름을
-	// 정의하여 프로젝트에 규칙을 부여합니다.
-
-	public Member loginMember(String id, String passwd);
-	public int insertMember(Member m);
-	public int updateMember(Member m);
-	public int deleteMember(String id);
-	public int checkIdDup(String id);
-}
-=======
 package com.dal.likeycakey.member.model.service;
 
 import com.dal.likeycakey.member.model.vo.Member;
@@ -23,4 +7,3 @@ public interface MemberService {
 	public Member loginMember(String id, String passwd) throws Exception;
 	public int idCheck(String id);
 }
->>>>>>> refs/heads/master
